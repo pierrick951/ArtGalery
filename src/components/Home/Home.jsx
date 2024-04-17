c0fd4ddfd674cd608c946aafd8821f6c2ff91f6d
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-04-17 14:59:09 -0400

    modification

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,24 +1,13 @@
-import { useEffect, useState } from 'react'
+
 import './Home.css'
-import sliderData from '../../data/sliderData'
-
+import artGallery from '../../../public/ArtGalleries.jpg'
 export default function Home() {
-  const [sliderIndex, setSliderIndex] = useState(1)
 
-  function toggleImg(indexPayload) {
-    if (indexPayload + state > sliderData.length) {
-      return 1
-    }else if (indexPayload + state < 1 ){
-      return sliderData.length
-    }else {
-      return state + indexPayload
-    }
-  }
   return (
     <div className="Container">
-      <img
-        className="slider-img"
-        src={`/public/img-${sliderIndex}.jpg`} alt="estate rooms" />
+      <img 
+      className='imgHeader'
+      src={artGallery} alt="" />
     </div>
   )
 }
