6ddcac793ab0dcc1cc7a8cf992c596a320434294
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-04-18 16:53:11 -0400

    ajout de modification de la carte  ala une

--- src/Layouts/HomeContainer2/HomeContainer2.jsx
+++ src/Layouts/HomeContainer2/HomeContainer2.jsx
@@ -2,6 +2,12 @@ import './HomeContainer2.css'
 
 export default function HomeContainer2() {
   return (
-    <div>HomeContainer2</div>
+    <div className='ContainerRigth'>
+      <h2>other</h2>
+      <ul>
+        <li>l'art de Basquiat</li>
+        <li>lets doodls with KEITH hARRING</li>
+      </ul>
+    </div>
   )
 }
