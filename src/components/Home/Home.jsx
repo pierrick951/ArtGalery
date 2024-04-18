c63fd866bdb667863786f66fa9e63212c8ef80f1
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-04-18 09:34:14 -0400

    ajout du container behind the header section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,9 +5,24 @@ export default function Home() {
   return (
     <div className="Container">
       <header>       
-      <h1>Art Museum  </h1>
+      <h1>Art Museum </h1>
       <p className='under-heading'>of</p>
       </header>
+
+
+      <main className='centerContainer'>
+        <section className='section1'>
+          <h2 className='heading2'>A la une</h2>
+         <div className="flexContainer">
+           <div className="firstGridBox">
+            <p>first box</p>
+           </div>
+           <div className="secondGridBox">
+            <p>second box</p>
+           </div>
+         </div>
+        </section>
+      </main>
     </div>
   )
 }
