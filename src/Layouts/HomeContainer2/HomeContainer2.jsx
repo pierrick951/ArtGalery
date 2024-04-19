3dc4878ba7a76cff3ced0396020b4af716c8c718
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-04-18 21:49:05 -0400

    modification

--- src/Layouts/HomeContainer2/HomeContainer2.jsx
+++ src/Layouts/HomeContainer2/HomeContainer2.jsx
@@ -1,13 +1,30 @@
 import './HomeContainer2.css'
+import  kuramata from '../../../public/kuramata.jpg'
+import ghibli from '../../../public/chihiro.jpg'
+import Basquiat from '../../../public/basquiat.jpg'
 
 export default function HomeContainer2() {
   return (
     <div className='ContainerRigth'>
       <h2>other</h2>
-      <ul>
-        <li>l'art de Basquiat</li>
-        <li>lets doodls with KEITH hARRING</li>
-      </ul>
+       <div className='ContainerR1'>
+        <img src={kuramata} alt="Kuramata" />
+        <div className="botR1">
+          <h3>Discover Shiro Kuramata</h3>
+        </div>
+       </div>
+       <div className='ContainerR1 reverse'>
+        <img src={ghibli} alt="Kuramata" />
+        <div className="botR1">
+          <h3>Art of studio Ghibli</h3>
+        </div>
+       </div>
+       <div className='ContainerR1 '>
+        <img src={Basquiat} alt="Kuramata" />
+        <div className="botR1">
+          <h3>Basquiat universe</h3>
+        </div>
+       </div>
     </div>
   )
 }
