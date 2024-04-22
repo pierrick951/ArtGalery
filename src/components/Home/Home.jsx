54bb53bb587f8903f0204ad63c7678fb3cf50e87
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-04-22 14:44:34 -0400

    ajout de style et image a la section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,27 +6,33 @@ export default function Home() {
 
   return (
     <div className="Container">
-      <header>       
-      <h1>Art Museum </h1>
-      <p className='under-heading'>of</p>
+      <header>
+        <h1>Art Museum </h1>
+        <p className='under-heading'>of</p>
       </header>
 
 
       <main className='centerContainer'>
         <section className='section1'>
           <h2 className='heading2'>A la une</h2>
-         <div className="flexContainer">
-           <div className="firstBox">
-             <HomeContainer1/>
-           </div>
-           
-         </div>
+          <div className="flexContainer">
+            <div className="firstBox">
+              <HomeContainer1 />
+            </div>
+
+          </div>
         </section>
         <section className='section2'>
-          <PresentationMusée/>
+          <PresentationMusée />
         </section>
-        <section className='paralaxseconde'>
-            
+
+        <section className='otherSection'>
+          <div className="boxBuns">
+            <div className='kuramata buns'> Kuramata</div>
+            <div className='keith buns reverse'>Keith Worlds</div>
+            <div className='buns basquiat '> Basquiat</div>
+            <div className='buns ghibli reverse'> Studio Ghibli</div>
+          </div>
         </section>
       </main>
     </div>
