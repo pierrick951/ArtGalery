0dfc7bc4e7ac05e20597b51edf0172d3226bdcd1
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-04-29 09:34:37 -0400

    ajout du foooter

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,40 @@ import './Footer.css'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+      <div>
+        <ul>
+          <h3>Discover</h3>
+        <li>lorem</li>
+        <li>lorem lorem</li>
+        <li>ipsum</li>
+        <li>dolor</li>
+        <li>dolor</li>
+        <li>ipsum dolor</li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <h3>Visit</h3>
+        <li>lorem</li>
+        <li>lorem lorem</li>
+        <li>ipsum</li>
+        <li>dolor</li>
+        <li>dolor</li>
+        <li>ipsum dolor</li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <h3>Tikets</h3>
+        <li>lorem</li>
+        <li>lorem lorem</li>
+        <li>ipsum</li>
+        <li>dolor</li>
+        <li>dolor</li>
+        <li>ipsum dolor</li>
+        </ul>
+      </div>
+    </div>
   )
-}
+} 
