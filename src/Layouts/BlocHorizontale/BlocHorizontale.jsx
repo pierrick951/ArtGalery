8b2f63c1f651389ed184f99b5ac68a7d4253b04b
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-05-03 18:04:24 -0400

    ajout de contenu

--- src/Layouts/BlocHorizontale/BlocHorizontale.jsx
+++ src/Layouts/BlocHorizontale/BlocHorizontale.jsx
@@ -1,11 +1,16 @@
 import './BlocHorizontale.css'
+import art from '../../assets/art.jpg'
 
 
 export default function BlocHorizontale() {
     return (
         <div className="boxHori">
             <div className="blocHori">
-                <div className="slide one">slide one</div>
+                <div className="slide one">
+                      <div>
+                        <img src={art} alt="" />
+                      </div>
+                </div>
                 <div className="slide two">slide  two </div>
                 <div className="slide three">slide tree</div>
             </div>
