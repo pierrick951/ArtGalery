95385c621ca2225ada00160c1f49846e192f7319
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-05-04 12:16:09 -0400

    modification de la struture horizontale et ajout d un nouveau fond pour le header sur la page home

--- src/Layouts/BlocHorizontale/BlocHorizontale.jsx
+++ src/Layouts/BlocHorizontale/BlocHorizontale.jsx
@@ -1,23 +1,22 @@
 import './BlocHorizontale.css'
 import art from '../../assets/art.jpg'
+import ahand from '../../assets/handArt.avif'
 
 
 export default function BlocHorizontale() {
-    return (
-        <div className="boxHori">
-            <div className="blocHori">
-                <div className="slide one">
-                      <div>
-                        <img src={art} alt="" />
-                      </div>
-                      <div className="wrapTxt">
-                        <h1>lorem</h1>
-                        <p>lor</p>
-                      </div>
-                </div>
-                <div className="slide two">slide  two </div>
-                <div className="slide three">slide tree</div>
-            </div>
+  return (
+    <div className="boxHori">
+      <div className="blocHori">
+        <div className="slide one">
+         <div className="oneBox1">
+          <img src={art} alt="" className="oneBox1__img" />
+          <h1>lorem ipsum</h1>
+         </div>
+
         </div>
-    )
+        <div className="slide two">slide  two </div>
+        <div className="slide three">slide tree</div>
+      </div>
+    </div>
+  )
 }
