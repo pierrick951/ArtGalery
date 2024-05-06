993a00af139971b5374a8cc650bbb302a9b57c8d
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-05-06 11:39:36 -0400

    debut de la slide 2 et modification de la slide 1

--- src/Layouts/BlocHorizontale/BlocHorizontale.jsx
+++ src/Layouts/BlocHorizontale/BlocHorizontale.jsx
@@ -1,6 +1,7 @@
 import './BlocHorizontale.css'
 import art from '../../assets/art.jpg'
 import ahand from '../../assets/handArt.avif'
+import glitch from '../../../public/glitch.jpg'
 
 
 export default function BlocHorizontale() {
@@ -8,17 +9,26 @@ export default function BlocHorizontale() {
     <div className="boxHori">
       <div className="blocHori">
         <div className="slide one">
-         <div className="oneBox1">
-          <img src={art} alt="" className="oneBox1__img" />
-          <h1>lorem ipsum</h1>
-         </div>
-         <div className='oneBox2'>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam blanditiis, similique exercitationem doloribus maiores quidem totam cumque .</p>
-          <img src={ahand} alt=""/>
-         </div>
+          <div className="oneBox1">
+            <img src={art} alt="" className="oneBox1__img" />
+            <h1>lorem ipsum</h1>
+            <h1>lorem ipsum</h1>
+            <h1>lorem ipsum</h1>
+          </div>
+          <div className='oneBox2'>
+            <p>매우 감사합니다. 엄청난 애무와 더 큰 고통을 수반하는 훈련, 참으로 그것의 전부입니다.</p>
+            <img src={ahand} alt="" />
+          </div>
 
         </div>
-        <div className="slide two">slide  two </div>
+        <div className="slide two">
+          <div className="twoBox1">
+            <img src={glitch} alt="" />
+          </div>
+          <div className="twoBox2">
+            <img src={glitch} alt="" />
+          </div>
+        </div>
         <div className="slide three">slide tree</div>
       </div>
     </div>
